c4b395f573452e62997c99b56139d6e6500303a5
ahargunyllib <billy.bpm03@gmail.com>
2026-01-07 18:38:41 +0700

    feat: Add MDX integration to Astro configuration

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://ahargunyllib.dev",
   adapter: cloudflare(),
-  integrations: [sitemap()],
+  integrations: [mdx(), sitemap()],
 });
