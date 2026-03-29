b3814ae41cf0052af6dd4e63a7fa2cca02b03a3b
ahargunyllib <billy.bpm03@gmail.com>
2026-03-29 17:52:16 +0700

    feat: update formatting and import statements across components; streamline pre-commit and linting scripts

--- apps/site/src/data/blogs.ts
+++
@@ -1,9 +0,0 @@
-import type { BlogPost } from "./types";
-
-export const blogPosts: BlogPost[] = [];
-
-// Get the most recent blog posts
-export const getRecentPosts = (limit = 3): BlogPost[] =>
-  blogPosts
-    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
-    .slice(0, limit);
