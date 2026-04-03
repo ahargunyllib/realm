976224cbbb5b3360781e775bd72ac0411d6b2b5a
ahargunyllib <billy.bpm03@gmail.com>
2026-04-03 23:58:50 +0700

    feat: enhance BaseContext type definition with detailed waitUntil documentation

--- packages/core/src/types/context.ts
+++ packages/core/src/types/context.ts
@@ -1,6 +1,12 @@
 import type { LoggerType } from "@realm/logger";
 
 export type BaseContext = {
+  /**
+   * This will be extends the lifetime of your Worker, allowing you to perform
+   * work without blocking returning a response, and that may continue after a response is returned.
+   * It accepts a Promise, which the Workers runtime will continue executing, even after a response has
+   * been returned by the Worker's handler.
+   */
   waitUntil: (promise: Promise<unknown>) => void;
   logger: LoggerType;
   requestId: string;
